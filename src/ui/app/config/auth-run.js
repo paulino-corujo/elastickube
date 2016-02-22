d246cc4b54c14df91be026a3a49c75d550253799
Manuel Martin <manuel@elasticbox.com>
2016-02-22 17:37:57 +0100

    Fixed initialisation

--- src/ui/app/config/auth-run.js
+++ src/ui/app/config/auth-run.js
@@ -1,19 +1,27 @@
-function checkRouteAccess($rootScope, auth, routerHelper) {
+function checkRouteAccess($rootScope, auth, routerHelper, initialization) {
     'ngInject';
 
     $rootScope.$on('$stateChangeStart', (event, toState, toParams, fromState) => {
-        if (!auth.authorize(toState.data.access)) {
-            event.preventDefault();
+        if (initialization.initialized) {
+            if (!auth.authorize(toState.data.access)) {
+                event.preventDefault();
 
-            if (fromState.url === '^') {
-                if (auth.isLoggedIn()) {
-                    const defaultNamespace = 'engineering';
+                if (fromState.url === '^') {
+                    if (auth.isLoggedIn()) {
+                        const defaultNamespace = 'engineering';
 
-                    routerHelper.changeToState('private.instances', { namespace: defaultNamespace });
-                } else {
-                    routerHelper.changeToState('anonymous.login');
+                        routerHelper.changeToState('private.instances', { namespace: defaultNamespace });
+                    } else {
+                        routerHelper.changeToState('anonymous.login');
+                    }
                 }
             }
+        } else {
+            event.preventDefault();
+
+            initialization.deferred.promise.then(() => {
+                routerHelper.changeToState(toState, toParams);
+            });
         }
     });
 }
