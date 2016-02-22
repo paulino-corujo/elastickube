1d7c4e5d24891709f6578e8b1a699a9b7962ba85
Manuel Martin <manuel@elasticbox.com>
2016-02-22 16:43:08 +0100

    Added admin namespaces view Added admin templates view Added admin instances view

---
+++ src/ui/app/admin/ek-admin-namespaces/ek-admin-namespaces.controller.js
@@ -0,0 +1,43 @@
+import rowTemplate from './ek-admin-namespaces-row.template.html';
+
+class AdminNamespacesController {
+    constructor($scope, namespacesStore) {
+        'ngInject';
+
+        this.bulkActions = 'Bulk Actions';
+        this.namespaces = namespacesStore.getAll();
+        this.filteredNamespaces = [];
+
+        this.gridOptions = {
+            rowTemplate,
+            data: 'ctrl.filteredNamespaces',
+            enableFiltering: false,
+            enableRowSelection: true,
+            enableSelectAll: true,
+            selectionRowHeaderWidth: 50,
+            rowHeight: 50,
+            showGridFooter: true,
+            columnDefs: [
+                {
+                    name: 'name',
+                    field: 'metadata.name',
+                    enableColumnMenu: false
+                },
+                { name: 'labels', field: 'metadata.labels', enableColumnMenu: false },
+                { name: 'members', enableColumnMenu: false },
+                { name: 'instances', enableColumnMenu: false }
+            ],
+            onRegisterApi: (gridApi) => {
+                this.gridApi = gridApi;
+
+                gridApi.selection.on.rowSelectionChanged($scope, () =>
+                    this.hasRowsSelected = !_.isEmpty(gridApi.selection.getSelectedRows()));
+
+                gridApi.selection.on.rowSelectionChangedBatch($scope, () =>
+                    this.hasRowsSelected = !_.isEmpty(gridApi.selection.getSelectedRows()));
+            }
+        };
+    }
+}
+
+export default AdminNamespacesController;
--- src/ui/app/admin/ek-admin-templates/ek-admin-templates.directive.js
+++ src/ui/app/admin/ek-admin-templates/ek-admin-templates.directive.js
@@ -1,16 +1,15 @@
 import './ek-admin-templates.less';
 import Directive from 'directive';
+import Controller from './ek-admin-templates.controller';
 import template from './ek-admin-templates.html';
 
 class AdminTemplatesDirective extends Directive {
     constructor() {
-        super({ template });
+        super({ Controller, template });
     }
 
     compile(tElement) {
-        tElement
-            .addClass('ek-admin-templates')
-            .attr('layout', 'column');
+        tElement.addClass('ek-admin-templates');
     }
 }
 
