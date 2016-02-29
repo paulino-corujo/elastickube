37aaf7c6a3370cc9cbb494f920b5ea3d4c72ebe9
Manuel Martin <manuel@elasticbox.com>
2016-02-29 22:25:20 +0100

    Fixes several style issues

--- src/ui/app/admin/icons.js
+++ src/ui/app/admin/icons.js
@@ -1,14 +1,18 @@
+import CHARTS from 'images/icon-charts.svg';
+import GOOGLE_G_LOGO from 'images/google-g-logo.svg';
 import IC_CLEAR_48PX from 'material-design-icons/content/svg/production/ic_clear_48px.svg';
 import IC_NAMESPACES_48PX from 'material-design-icons/social/svg/production/ic_public_48px.svg';
+import IC_PERSON_48PX from 'material-design-icons/social/svg/production/ic_person_48px.svg';
 import IC_SETTINGS_48PX from 'material-design-icons/action/svg/production/ic_settings_48px.svg';
 import IC_USERS_48PX from 'material-design-icons/social/svg/production/ic_group_48px.svg';
 import INSTANCES from 'images/icon-instances.svg';
-import CHARTS from 'images/icon-charts.svg';
 
 const icons = {
     CHARTS,
+    GOOGLE_G_LOGO,
     IC_CLEAR_48PX,
     IC_NAMESPACES_48PX,
+    IC_PERSON_48PX,
     IC_SETTINGS_48PX,
     IC_USERS_48PX,
     INSTANCES
