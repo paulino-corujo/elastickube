2cfa1679a327c8a069a41fa5940780238b9bf5dd
Manuel Martin <manuel@elasticbox.com>
2016-02-29 21:47:41 +0100

    Added missing icons in admin menu.

--- src/ui/app/admin/icons.js
+++ src/ui/app/admin/icons.js
@@ -2,12 +2,16 @@ import IC_CLEAR_48PX from 'material-design-icons/content/svg/production/ic_clear
 import IC_NAMESPACES_48PX from 'material-design-icons/social/svg/production/ic_public_48px.svg';
 import IC_SETTINGS_48PX from 'material-design-icons/action/svg/production/ic_settings_48px.svg';
 import IC_USERS_48PX from 'material-design-icons/social/svg/production/ic_group_48px.svg';
+import INSTANCES from 'images/icon-instances.svg';
+import CHARTS from 'images/icon-charts.svg';
 
 const icons = {
+    CHARTS,
     IC_CLEAR_48PX,
     IC_NAMESPACES_48PX,
     IC_SETTINGS_48PX,
-    IC_USERS_48PX
+    IC_USERS_48PX,
+    INSTANCES
 };
 
 export default icons;
