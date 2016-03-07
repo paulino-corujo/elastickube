ec7c49b53e50b0287b9a12774411bea994057916
Paulino Corujo Huerta <paulino@elasticbox.com>
2016-03-07 12:55:08 +0100

    Deploy chart from chart grid view

--- src/ui/app/charts/navigation-action-creator.service.js
+++ src/ui/app/charts/navigation-action-creator.service.js
@@ -9,6 +9,10 @@ class NavigationActionCreatorService {
     charts(namespace = this._sessionStore.getActiveNamespace()) {
         return this._routerHelper.changeToState('charts', { namespace: namespace.metadata.name });
     }
+
+    deployChart(chart) {
+        return this._routerHelper.changeToState('new-instance', { chart });
+    }
 }
 
 export default NavigationActionCreatorService;
