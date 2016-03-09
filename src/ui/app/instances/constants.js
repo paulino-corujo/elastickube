6f952b85e13eee6a4092bd6c154753126a0cc641
Alberto Arias Maestro <alberto@elasticbox.com>
2016-03-09 12:10:33 -0800

    Add copyright notice to source code

--- src/ui/app/instances/constants.js
+++ src/ui/app/instances/constants.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2016 ElasticBox All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 import IC_ADD_CIRCLE_OUTLINE_48PX from 'material-design-icons/content/svg/production/ic_add_circle_outline_48px.svg';
 import IC_ARROW_DROP_DOWN_48PX from 'material-design-icons/navigation/svg/production/ic_arrow_drop_down_48px.svg';
 import IC_CREATE_48PX from 'material-design-icons/content/svg/production/ic_create_48px.svg';
